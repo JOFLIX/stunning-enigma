fcc9659ab49027cafa0d50ae31cb8c3941f9887e
JOFLIX <joflixooko@outlook.com>
2019-09-20 04:39:37 +0300

    deployment

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ class TestConfig(Config):
     DEBUG =True
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa-school:123@localhost/blogs'
+    SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL")
     DEBUG = True
 
 config_options = {
